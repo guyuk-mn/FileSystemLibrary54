b44894e420e15bbd6a13e2ae5f97c55964b78103
1123581321345589144233344 <byambag12@gmail.com>
2025-02-01 16:53:53 +0800

    fixed header reference

--- Source/FileSystemLibrary/Public/FileSystemLibraryBPLibrary.h
+++ Source/FileSystemLibrary/Public/FileSystemLibraryBPLibrary.h
@@ -23,7 +23,7 @@
 #include "Engine/Engine.h"
 #include "Engine/GameViewportClient.h"
 #include "HAL/FileManager.h"
-#include "HAL/PlatformFilemanager.h"
+#include "HAL/PlatformFileManager.h"
 #include "Misc/Paths.h"
 #include "Misc/FileHelper.h"
 #include "GenericPlatform/GenericPlatformFile.h"
